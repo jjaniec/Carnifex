1166812372c9a41efd539e16f64808a5019eb9e2
Joffrey Janiec <jjaniec@student.42.fr>
2018-03-02 20:20:15 +0100

    init travis tests on sbcl

--- main.lisp
+++ main.lisp
@@ -1,6 +1,6 @@
-(load 'srcs/ft_clear_screen.lisp)
-(load 'srcs/ft_gen_arr.lisp)
-(load 'srcs/cli.lisp)
+(load "srcs/ft_clear_screen.lisp")
+(load "srcs/ft_gen_arr.lisp")
+(load "srcs/cli.lisp")
 
 (ft_clear_screen_cli)
 (ft_gen_arr 10 10)
@@ -9,3 +9,5 @@
 (setf (aref arr 7 2) 1)
 (write arr)
 (ft_print_cli_board arr 10 10)
+
+(exit)
