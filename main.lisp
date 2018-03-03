5889f1606a4ebb058f87ca7cc5c076d74b5d76cd
Joffrey Janiec <jjaniec@student.42.fr>
2018-03-03 20:18:01 +0100

    auto-adapt tile_sizes and offsets between tiles for zoom

--- main.lisp
+++ main.lisp
@@ -4,10 +4,11 @@
 (defun main ()
   (setq y 100)
   (setq x 100)
-  (setq zoom 1)
+  (setq tile_size 15)
+  (setq zoom_dec_speed 2)
   (setq arr (make-array (list y x) :initial-element 0))
 
-  (ft_loop)
+  (ft_loop tile_size)
   (exit)
 )
 
