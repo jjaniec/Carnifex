2b20adda5851c783d1321a62b0381fb6f3a7c80d
tim <tnicolas@student.42.fr>
2018-03-03 20:18:13 +0100

    le deplacement

--- main.lisp
+++ main.lisp
@@ -4,7 +4,12 @@
 (defun main ()
   (setq y 100)
   (setq x 100)
+  (setq width 1000)
+  (setq height 1000)
   (setq zoom 1)
+  (defparameter move_x 0)
+  (defparameter move_y 0)
+  (setq move_speed 10)
   (setq arr (make-array (list y x) :initial-element 0))
 
   (ft_loop)
