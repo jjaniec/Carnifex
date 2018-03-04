f0537d251d1aae494f17ff1f2c037751e4e919b6
Clement Trosa <me@trosa.io>
2018-03-04 19:14:36 +0100

    Only Tom Cruise knows why.

--- main.lisp
+++ main.lisp
@@ -3,11 +3,13 @@
 (load "srcs/cli.lisp")
 (load "srcs/extra.lisp")
 
-;;(main *posix-argv*))
 (defun main ()
-  (ft_usage :pname (car sb-ext:*posix-argv*))
-	(setq y (parse-integer (nth 1 *posix-argv*)))
-  (setq x (parse-integer (nth 2 *posix-argv*)))
+  (ft_usage :pname
+    (car sb-ext:*posix-argv*))
+  (setq y (parse-integer
+    (nth 1 *posix-argv*)))
+  (setq x (parse-integer
+    (nth 2 *posix-argv*)))
   (setq width 1000)
   (setq height 1000)
   (setq zoom_dec_speed 2)
@@ -15,7 +17,6 @@
   (defparameter tile_size 15)
   (defparameter last_x 0)
   (defparameter last_y 0)
-
   (defparameter move_x 0)
   (defparameter move_y 0)
   (defparameter pause 1)
@@ -23,13 +24,13 @@
   (setq change_speed_game 5)
   (defparameter cur_time 0)
   (defparameter last_time 0)
-  
-  (setq arr (make-array (list y x) :initial-element 0))
-  (setq next_generation (make-array (list y x) :initial-element 0))
+  (setq arr (make-array
+    (list y x) :initial-element 0))
+  (setq next_generation (make-array
+    (list y x) :initial-element 0))
   ;;(setq arr (rand)) <-- random mode
-
   (ft_loop)
-  (exit)
-)
+  (exit))
 
-(sb-int:with-float-traps-masked (:invalid :inexact :overflow) (main))
+(sb-int:with-float-traps-masked
+ (:invalid :inexact :overflow) (main))
--- srcs/cli.lisp
+++ srcs/cli.lisp
@@ -1,9 +1,9 @@
 (defun ft_print_cli_board (arr x y)
   (terpri)
   (loop for y2 from 0 to (- y 1) do
-    (loop for x2 from 0 to (- x 1) do
-      (if (eq (eq (aref arr y2 x2) 0) T)
-        (write '-))
-      (if (eq (eq (aref arr y2 x2) 1) T)
-        (write-char #\#)))
-    (terpri)))
+		(loop for x2 from 0 to (- x 1) do
+			  (if (eq (eq (aref arr y2 x2) 0) T)
+				  (write '-))
+			  (if (eq (eq (aref arr y2 x2) 1) T)
+				  (write-char #\#)))
+		(terpri)))
